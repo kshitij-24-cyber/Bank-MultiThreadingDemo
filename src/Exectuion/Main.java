fb4b02ff1acbb70b7fdf43331d1889ccbe8b1574
dbc2201 <kshitij.sahu_cs19@gla.ac.in>
2022-02-20 01:00:54 +0530

    eod

--- src/Exectuion/Main.java
+++ src/Exectuion/Main.java
@@ -7,27 +7,34 @@ package Exectuion;/*
 import Defination.Bank;
 
 public class Main {
-    public static void main(String[] args) {
-        Bank accoutnt1 = new Bank(1, 1000);
+    public static void main(String[] args) throws InterruptedException {
+        Bank account1 = new Bank(1, 1000);
         Bank accoutnt2 = new Bank(2, 2000);
 
         myThread[] thread = new myThread[6];
         //thread for account1
-        thread[1] = new myThread(accoutnt1, true, 500);
-        thread[2] = new myThread(accoutnt1, false, 300);
-        thread[3] = new myThread(accoutnt1, true, 2500);
+        thread[0] = new myThread(account1, true, 500);
+        thread[1] = new myThread(account1, false, 300);
+        thread[2] = new myThread(account1, true, 2500);
 
         //threads for account2
-        thread[4] = new myThread(accoutnt2, true, 1500);
-        thread[5] = new myThread(accoutnt2, true, 5200);
-        thread[6] = new myThread(accoutnt2, false, 2500);
+        thread[3] = new myThread(accoutnt2, true, 1500);
+        thread[4] = new myThread(accoutnt2, true, 5200);
+        thread[5] = new myThread(accoutnt2, false, 2500);
 
         // loop for starting thread
         for (int threads = 0; threads <6 ; threads++) {
+            thread[threads].start();
 
         }
         //loop for completion of threads
+        for (int threads = 0; threads <6 ; threads++) {
+            thread[threads].join();
+        }
 
+
+        System.out.println( "Amount in account 1:" + account1.getAccountBalance());
+        System.out.println( "Amount in account :" +accoutnt2.getAccountBalance());
     }
 
     public static class myThread extends Thread {
@@ -49,7 +56,7 @@ public class Main {
                 withdraw();
             }
         }
-
+ 
         private void deposit() {
             this.account.setAccountBalance(this.account.getAccountBalance() + this.amount);
             try {
