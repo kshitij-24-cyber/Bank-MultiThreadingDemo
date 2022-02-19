ec55bf8ea2baea9cd6436a8f42845ca6b3b04bdd
dbc2201 <kshitij.sahu_cs19@gla.ac.in>
2022-02-20 00:52:03 +0530

    initial commit

--- src/Exectuion/Main.java
+++ src/Exectuion/Main.java
@@ -10,6 +10,24 @@ public class Main {
     public static void main(String[] args) {
         Bank accoutnt1 = new Bank(1, 1000);
         Bank accoutnt2 = new Bank(2, 2000);
+
+        myThread[] thread = new myThread[6];
+        //thread for account1
+        thread[1] = new myThread(accoutnt1, true, 500);
+        thread[2] = new myThread(accoutnt1, false, 300);
+        thread[3] = new myThread(accoutnt1, true, 2500);
+
+        //threads for account2
+        thread[4] = new myThread(accoutnt2, true, 1500);
+        thread[5] = new myThread(accoutnt2, true, 5200);
+        thread[6] = new myThread(accoutnt2, false, 2500);
+
+        // loop for starting thread
+        for (int threads = 0; threads <6 ; threads++) {
+
+        }
+        //loop for completion of threads
+
     }
 
     public static class myThread extends Thread {
@@ -26,23 +44,28 @@ public class Main {
         @Override
         public void run() {
             if (isDepost) {
-                try {
-                    deposit();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                deposit();
             } else {
                 withdraw();
             }
         }
 
-        private void deposit() throws InterruptedException {
+        private void deposit() {
             this.account.setAccountBalance(this.account.getAccountBalance() + this.amount);
-            myThread.sleep(2000);
+            try {
+                myThread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
         private void withdraw() {
             this.account.setAccountBalance(this.account.getAccountBalance() - this.amount);
+            try {
+                myThread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
